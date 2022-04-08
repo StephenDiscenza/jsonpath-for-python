6a8c9c4936fe39efab8b107bfb46a2051f74dded
Steve <stephendiscenza@gmail.com>
2022-04-08 12:22:40 -0400

    added main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jsonpath-for-python
-A little utility which parses and uses JSONPath expressions to do things with Python data structures 
+A little utility which parses and uses JSONPath expressions to do things with Python data structures representing a JSON document.
+
+Does not currently support multiple field matching. This is on the todo list.
---
+++ main.py
@@ -0,0 +1,74 @@
+import re
+
+
+def search_json(jsonDocument, pathItems):
+    '''
+    Uses parsed JSONPath components to recursively search though 
+    Python lists and dicts which represent a valid JSON structure.
+    '''
+    if len(pathItems) == 0:
+        return jsonDocument
+    pathInfo = re.findall(r"[\w\s\d\-\+\%\']+", pathItems[0])
+    if len(pathInfo) == 1: # searching a dict
+        jsonDocument = jsonDocument.get(pathInfo[0])
+
+    elif len(pathInfo) == 2: # searching a list and we have the index
+        field, index = pathInfo
+        jsonDocument = jsonDocument.get(field)[int(index)]
+    
+    elif len(pathInfo) == 3: # searching a list and we have a subfield/value pair like [?subfield="value"]
+        field, subfield, value = pathInfo
+        value = value.lstrip("'").rstrip("'")
+        if type(jsonDocument.get(field)) != list:
+            raise Exception(f'Cannot query {jsonDocument.get(field)} with ?{subfield}={value}')
+        for item in jsonDocument.get(field):
+            if str(item.get(subfield)) == value:
+                jsonDocument = item
+                return search_json(jsonDocument, pathItems[1:])
+        return None #If item not in list
+    
+    return search_json(jsonDocument, pathItems[1:])
+
+
+def get_json_request_item(jsonDocument, path):
+    '''
+    Uses supplied JSONPath to search for and return a value or 
+    object from the JSON document. 
+    '''
+    try:
+        pathItems = path.split(".")[1:]
+        result = search_json(jsonDocument, pathItems)
+    except IndexError:
+        result = None
+    except:
+        raise Exception(f'An unexpected error occured in get_json_request_item. The path was: {path}')
+    else:
+        return result
+
+
+def update_json_element(jsonDocument, path, value):
+    '''
+    Upserts the value of the field specified by the supplied path.
+    '''
+    try:
+        pathItems = path.split(".")[1:]
+        containingDict = search_json(jsonDocument, pathItems[:-1])
+        containingDict[pathItems[-1]] = value
+    except:
+        raise Exception('Dang, update failed for path: {path}')
+
+
+def write_new_json_element(jsonDocument, path, newElementName, value):
+    '''
+    Inserts new data into the JSON document. If the special value "newArray" is
+    passed it will create a new empty array with the supplied name. When inserting
+    into a list it will add an object like {"Cd": newElementName, "Value": value}
+    '''
+    pathItems = path.split(".")[1:]
+    insertLocation = search_json(jsonDocument, pathItems)
+    if value == 'newArray':
+        insertLocation[newElementName] = []
+    elif path[-1] != ']' and len(path) > 1:
+        insertLocation.append({'Cd': newElementName, 'Value': value})
+    else:
+        insertLocation[newElementName] = value
